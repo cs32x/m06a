50c3975f233b40443567d125f6e77fce4a4b4492
Mike Smith <48131840+profsmith89@users.noreply.github.com>
2026-06-23 14:00:01 +0000

    Add pseudocode that starts the design

---
+++ qweb0.py
@@ -0,0 +1,4 @@
+# Craft a request Wikipedia will understand about _The Cat in the Hat_
+# Send that request from our computers to the one running Wikipedia
+# Read the response from the Wikipedia computer
+# Print the answer to our question
